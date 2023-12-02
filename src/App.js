c993d4735075502664eb49f3e79dc205f77e0a1b
ajay sonere <ajaysonere786@gmail.com>
2023-12-02 13:20:41 +0530

    tech stack section added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import './home.css';
+import Techstack from "./components/Techstack";
 
 const App = () => {
     return (
        <>
           <Navbar />
           <Hero />
+          <Techstack />
        </>
     );
 };
